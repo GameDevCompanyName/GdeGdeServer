6393e76023c68ae8b23bd7ba2aaf240d83c81301
Igorlo <9440533@gmail.com>
2019-06-18 04:39:22 +0300

    Improvements

--- src/main/kotlin/ru/gdcn/gdegde/Achievement.kt
+++ src/main/kotlin/ru/gdcn/gdegde/Achievement.kt
@@ -1,6 +1,16 @@
 package ru.gdcn.gdegde
 
-data class Achievement(val title: String, val description: String){
+import ru.gdcn.gdegde.database.DBEntity
+
+data class Achievement(val id: Int, val title: String, val description: String) : DBEntity{
+
+    override fun getValuesMap(): Map<String, Any> {
+        val map = HashMap<String, Any>()
+        map["id"] = id
+        map["title"] = title
+        map["description"] = description
+        return map
+    }
 
     override fun toString(): String {
         return "Достижение : $title\nОписание : $description"
